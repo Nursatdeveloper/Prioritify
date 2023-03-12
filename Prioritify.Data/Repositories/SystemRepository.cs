48b28eadb73e4a5c2e0244ddb1442f95a3e50c5b
Nursat Zeinolla <kznursat@gmail.com>
2023-03-12 21:33:43 +0600

    added SystemDbContext, and SystemRepository

---
+++ Prioritify.Data/Repositories/SystemRepository.cs
@@ -0,0 +1,33 @@
+﻿using Prioritify.Data.DbContexts;
+using System.Linq.Expressions;
+
+namespace Prioritify.Data.Repositories {
+    public interface ISystemRepository<T> where T : class {
+        Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        Task InsertAsync(T entity);
+    }
+    public class SystemRepository<T>: ISystemRepository<T> where T : class {
+        private readonly SystemDbContext _context;
+        public SystemRepository(SystemDbContext context) {
+            _context = context;
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter) {
+            try {
+                var entity = await _context.Set<T>().FindAsync(filter);
+                return entity;
+            }catch(Exception ex) {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task InsertAsync(T entity) {
+            try {
+                await _context.Set<T>().AddAsync(entity);
+                await _context.SaveChangesAsync();
+            } catch(Exception ex) {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
